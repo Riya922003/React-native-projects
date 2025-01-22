2ed46e38d94d4dfbed6e80dace378a0561d820c8
Riya Gupta <139098932+Riya922003@users.noreply.github.com>
2025-01-22 23:26:16 +0530

    Add files via upload

---
+++ Book-reading-app/constants/icons.ts
@@ -0,0 +1,13 @@
+// import google from "../assets/icons/google.png";
+// import home-icon from "../assets/icons/home-icon.png";
+// import lib-icon from "../assets/icons/lib-icon.png";
+// import search-icon from "../assets/icons/search-icon.png";
+// import user-icon from "../assets/icons/user-icon.png";
+
+// export default{
+//     google,
+//     home-icon,
+//     lib-icon,
+//     search-icon,
+//     user-icon,
+// };
---
+++ Book-reading-app/constants/images.ts
@@ -0,0 +1,29 @@
+import backslider2 from './assets/images/backslider2.png';
+import backslider3 from './assets/images/backslider3.png';
+import hero from './assets/images/hero.png';
+import main1 from './assets/images/main1.png';
+import main2 from './assets/images/main2.png';
+import main3 from './assets/images/main3.png';
+import main11 from './assets/images/main11.png';
+import main22 from './assets/images/main22.png';
+import main33 from './assets/images/main33.png';
+import slider1 from './assets/images/slider1.png';
+import slider2 from './assets/images/slider2.png';
+import slider3 from './assets/images/slider3.png';
+import sliderback1 from './assets/images/sliderback1.png';
+
+export default {
+  backslider2,
+  backslider3,
+  hero,
+  main1,
+  main2,
+  main3,
+  main11,
+  main22,
+  main33,
+  slider1,
+  slider2,
+  slider3,
+  sliderback1,
+};
